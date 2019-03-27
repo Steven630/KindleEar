b5990cc3f46a16247e3128900a190939cdaae3d4
Steven630 <64087266@163.com>
2019-03-27 05:29:56 +0000

    detour

--- books/WardofTerror.py
+++ books/WardofTerror.py
@@ -1,69 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from bs4 import BeautifulSoup
-from base import BaseFeedBook, URLOpener, string_of_tag
+from base import BaseFeedBook
 
 def getBook():
-    return Ward
-    
+    return WeixinRss
 
-class Ward(BaseFeedBook):
-    title                 =  u'惊人院'
-    description           =  u'惊人院微信'
+class WeixinRss(BaseFeedBook):
+    title                 = u'微信'
+    description           = u'微信RSS'
     language              = 'zh-cn'
     feed_encoding         = "utf-8"
     page_encoding         = "utf-8"
-    mastheadfile          = "mh_economist.gif" 
-    coverfile             = "cv_economist.jpg"
-    oldest_article        = 1
-#    deliver_days          = ['Friday']
-#    deliver_times         = [18]
-    fulltext_by_readability = False
-    keep_image            =  False
+    mastheadfile          = "mh_ftchinese.gif"
+    coverfile             = "cv_ftchinese.jpg"
+    oldest_article        = 3
     
-    keep_only_tags = [
-                      dict(name='h1'),
-                      dict(id='js_content')
-                     ]
-#     remove_tags_after =[dict(name='section', attrs={'style': re.compile('box-sizing: border-box; background-color: rgb(255, 255, 255);')})]
-#     remove_tags = [dict(name='section', attrs={'style': re.compile('box-sizing: border-box; background-color: rgb(255, 255, 255);')})]
-    remove_tags_after =[dict(name='strong', string=u'·END·')]
-    remove_tags = [dict(name='strong', string=u'·END·')]
-    
-    
-#    remove_tags = [{'name':'section'}]
-    
-    def ParseFeedUrls(self):
-        #return lists like [(section,title,url,desc),..]
-        main = 'http://www.jintiankansha.me/column/PQ9tpUoank'
-        urls = []
-        opener = URLOpener(self.host, timeout=90)
-        result = opener.open(main)
-        if result.status_code != 200:
-            self.log.warn('fetch webpage failed:%s'%main)
-            return []
-            
-        content = result.content.decode(self.feed_encoding)
-        soup = BeautifulSoup(content, "lxml")
-        
-        #开始解析
-        section = soup.find('div', class_='entries')
-        for article in section.find_all('div', class_='cell item', limit=10):
-            timestamp = article.find('span', class_='small fade')
-            timestamp = string_of_tag(timestamp).strip()
-#            if u'小时' not in timestamp and u'昨天' not in timestamp:
-            if u'小时' not in timestamp:
-                continue
-            span = article.find('span', class_='item_title')
-            a = span.find('a', href=True)
-            title = string_of_tag(a).strip()
-            if not title:
-                self.log.warn('This title not found.')
-                continue
-            if u'[原创]' not in title:
-                continue
-            url = a['href']
-            urls.append((u'惊人院',title,url,None))
-        if len(urls) == 0:
-            self.log.warn('len of urls is zero.')
-        return urls
+    feeds = [
+            (u'微信', 'https://werss.app/api/v1/xfeeds/c74ada88-6a8b-4317-8fdf-8335436f8f27.xml', True),
+            ]
+
+    keep_image = False
