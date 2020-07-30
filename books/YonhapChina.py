94c76c589bf2cf26efd8dbb0ec4ad9547a1764d7
Steven630 <64087266@163.com>
2020-07-30 16:05:20 +0800

    cycle

--- books/YonhapChina.py
+++ books/YonhapChina.py
@@ -40,43 +40,49 @@ class YonhapChina(BaseFeedBook):
     
     def ParseFeedUrls(self):
         #return lists like [(section,title,url,desc),..]
-        main = 'https://www.yna.co.kr/international/china'
+        mainhead = 'https://www.yna.co.kr/international/china/'
+        num =1
         urls = []
-        opener = URLOpener(self.host, timeout=90)
-        result = opener.open(main)
-        if result.status_code != 200:
-            self.log.warn('fetch mainnews failed:%s'%main)
-            
-        content = result.content.decode(self.page_encoding)
-        soup = BeautifulSoup(content, "lxml")
-        
+        calllitaday = False
         koreanow = datetime.datetime.utcnow()+ datetime.timedelta(hours=9)
         koreadate = koreanow.date()
         year = koreanow.year
-        #开始解析
         
-        section = soup.find('div', class_='list-type038')
-        for article in section.find_all('div', class_='item-box01'):
-            if article is None:
-                self.log.warn('This article not found')
-                continue
-            ptime= article.find('span', class_='txt-time')
-            if ptime:
-                ptime= string_of_tag(ptime).strip()
-                pdate=ptime[0:5] #只要07-30这样的日期
-                pdate= str(year) + '-'+ pdate #加上年份，否则默认1900年
-                pdate = datetime.datetime.strptime(pdate, '%Y-%m-%d').date()
-                delta=(koreadate-pdate).days
-                if self.oldest_article > 0 and delta >= self.oldest_article:
+        while not callitaday:
+            main = mainhead + str(num)
+            opener = URLOpener(self.host, timeout=90)
+            result = opener.open(main)
+            if result.status_code != 200:
+                self.log.warn('fetch mainnews failed:%s'%main)
+
+            content = result.content.decode(self.page_encoding)
+            soup = BeautifulSoup(content, "lxml")
+            #开始解析
+
+            section = soup.find('div', class_='list-type038')
+            for article in section.find_all('div', class_='item-box01'):
+                if article is None:
+                    self.log.warn('This article not found')
                     continue
-            newscon = article.find('div', class_='news-con')
-            a = newscon.find('a', href=True)
-            atitle = string_of_tag(a).strip()
-            atitle = atitle + ' ' + ptime[6:] #只保留点钟
-            url = a['href']
-            if url.startswith('/'):
-                url= 'https:'+ url
-            urls.append((u'중국 뉴스',atitle,url,None))        
+                ptime= article.find('span', class_='txt-time')
+                if ptime:
+                    ptime= string_of_tag(ptime).strip()
+                    pdate=ptime[0:5] #只要07-30这样的日期
+                    pdate= str(year) + '-'+ pdate #加上年份，否则默认1900年
+                    pdate = datetime.datetime.strptime(pdate, '%Y-%m-%d').date()
+                    delta=(koreadate-pdate).days
+                    if self.oldest_article > 0 and delta >= self.oldest_article:
+                        callitaday = True
+                        break #因为是按时间顺序的
+                newscon = article.find('div', class_='news-con')
+                a = newscon.find('a', href=True)
+                atitle = string_of_tag(a).strip()
+                atitle = atitle + ' ' + ptime[6:] #只保留点钟
+                url = a['href']
+                if url.startswith('/'):
+                    url= 'https:'+ url
+                urls.append((u'중국 뉴스',atitle,url,None))
+            num = num + 1
         if len(urls) == 0:
             self.log.warn('len of urls is zero.')
         return urls
